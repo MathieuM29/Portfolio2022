1870b68c70fe54c6ed36f639bc6cf072b676c01d
MathieuM29 <marcmathieu29@gmail.com>
2022-02-16 14:24:37 +0100

    config modele and first component

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,14 @@
+// == Import
+import './styles.scss';
+
+
+
+// == Composant
+const Portfolio = () => (
+  <div className="portfolio">
+    
+  </div>
+);
+
+// == Export
+export default Portfolio;
---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import {
+
+} from '../actions';
+
+const stateInitial = {
+  
+};
+
+export default (state = stateInitial, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
