a4acd548308994ef1d23fd9184c4d17f0fa93237
MathieuM29 <marcmathieu29@gmail.com>
2022-06-13 13:01:14 +0200

    desktop responsive root and portfolio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,26 @@
 // == Import
 import './styles.scss';
+import { useMediaQuery } from 'react-responsive';
 import SubHeader from './SubHeader';
 import Menu from './Menu';
 
 // == Composant
 const Header = () => {
 
+  const isTablet = useMediaQuery({
+    query: '(min-width: 421px)',
+    queryMax: '(max-width: 1024px)',
+  });
+
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1025px)',
+    queryMax: '(max-width: 1920px)',
+  });
+
   return (
 
   <header className="header">
-    <SubHeader />
+    {(!isTablet && !isDesktop) && <SubHeader />}
     <Menu />
   </header>
   );
